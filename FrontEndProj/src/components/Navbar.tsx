5b4356e66ce1da090d8d1e6cb999f19e630df47a
patelpradyot <patelpradyot@gmail.com>
2025-02-21 09:53:45 +0530

    chnage in navbar

--- FrontEndProj/src/components/Navbar.tsx
+++ FrontEndProj/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import "../styles/navbar.css";
 
 const Navbar = () => {
@@ -8,17 +9,21 @@ const Navbar = () => {
 
     return (
         <nav className="navbar">
-            <div>Dodgebot</div>
-            <div className="nav-links">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/pricing" className="nav-link">Pricing</Link>
-                <Link to="/solutions" className="nav-link">Solutions</Link>
-                <Link to="/case-studies" className="nav-link">Case Studies</Link>
-                <Link to="/contact" className="nav-link">Contact</Link>
-                <Link to="/happy-clients" className="nav-link">Happy Clients</Link>
+            <div className="logo">Dodgebot</div>
+
+            {/* Desktop Navigation */}
+            <div className={`nav-links ${mobileOpen ? "open" : ""}`}>
+                <Link to="/" className="nav-link" onClick={() => setMobileOpen(false)}>Home</Link>
+                <Link to="/pricing" className="nav-link" onClick={() => setMobileOpen(false)}>Pricing</Link>
+                <Link to="/solutions" className="nav-link" onClick={() => setMobileOpen(false)}>Solutions</Link>
+                <Link to="/case-studies" className="nav-link" onClick={() => setMobileOpen(false)}>Case Studies</Link>
+                <Link to="/contact" className="nav-link" onClick={() => setMobileOpen(false)}>Contact</Link>
+                <Link to="/happy-clients" className="nav-link" onClick={() => setMobileOpen(false)}>Happy Clients</Link>
             </div>
+
+            {/* Mobile Menu Icon */}
             <div className="menu-icon" onClick={() => setMobileOpen(!mobileOpen)}>
-                <MenuIcon />
+                {mobileOpen ? <CloseIcon /> : <MenuIcon />}
             </div>
         </nav>
     );
